2bc757b7646d2d0cbfc441683f45af6ae7fa04b8
Giltaehyeong <r1605866@naver.com>
2022-04-27 21:27:25 +0900

    [스프링 부트 입문 10]롬복과 리팩터링 * 롬복을 활용하여, 기존 코드를 "리팩터링" 하시오.
    1. 롬복이란
    코드를 간소화 시켜주는 라이브러리
    Getter(), setter(), constructor(), toString()...
    일일이 귀찮게 만들기 싫어서 나온 도구
    
    2. 롬복의 기능
    필수 코드의 반복을 최소화할 수 있고,
    Logging 기능을 통해 개선, 즉 "리팩터링" 할 수 있다.
    
    Refactoring 이란?
    코드의 구조 또는 성능을 개선하는 작업
    
    Logging 이란?
    프로그램의 수행과정을 기록으로 남기는것, 자동차 블랙박스!
    
    3. 기존 코드 확인
    
    4. 실습 개요
    @Data, @Slf4j
    롬복과 로깅
    
    5. 롬복 라이브러리 추가
    build.gradle에 dependencies{ 안에
    롬복 라이브러리 두줄 추가
    compileOnly 'org.projectlombok:lombok'
    annotationProcessor 'org.projectlombok:lombok'
    후 코끼리 새로고침 해주기.(Load Gradle Changes)
    
    6. 롬복 플러그인 설치
    help - Find Action - plugins - Lombok 검색 - install
    ! -> Lombok Requires Annotation Processing 이 필요하다.
    Do you want to enable annotation processors?. 메세지 출력
    //주석 프로세서를 활성화하시겠습니까?
    enable 체크
    못했을 시에
    File - Settings - Build, Execution, Deployment - Compiler - Annotation Processors
    ㅁ Enable annotation processing 체크
    // 주석 처리 활성화
    
    7. DTO 리팩토링
    기존 생성자와, toString() 객체 지우고
    @AllArgsConstructor //생성자
    @ToString // toString()
    public class ArticleForm { 위에 적어준다.
    이 과정을 Refactoring 개선했네 ~
    
    8. ENTITY 리팩토링
    기존 생성자와, toString() 객체 지우고
    @AllArgsConstructor //생성자
    @ToString // toString()
    public class ArticleForm { 위에 적어준다.
    이 과정을 Refactoring 개선했네 ~
    
    9. 로그 남기기
    System.out.println(form.toString()); -> 로깅기능으로 대체!
    - 로그 기록에도 남지않고, 서버 성능에도 안좋음, 그래서 로깅을 써야함
    log.info(form.toString()); 요런식으로
    
    9-1. 로깅이란?
    사고날때 블랙박스가 영상들을 다 기록하듯이
    서버에서 일어나는 일들을 다 기록할 수 있게 하는것.
    
    9-2. 로깅 사용법.
    @Slf4j // 로깅을 위한 골뱅이(어노테이션) 추가
    log.info(form.toString()); 요런식으로 선언
    
    10. 서버 실행시 주의점
    서버 두개 실행시 오류나니깐 확인해보고 실행해라 ~
    2022-04-27 21:25:42.545  INFO 10976 --- [nio-8080-exec-3] c.e.f.controller.ArticleController       : ArticleForm(title=가가가가가가가, content=나나나나나나나)
    2022-04-27 21:25:42.559  INFO 10976 --- [nio-8080-exec-3] c.e.f.controller.ArticleController       : Article(id=null, title=가가가가가가가, content=나나나나나나나)
    2022-04-27 21:25:42.654  INFO 10976 --- [nio-8080-exec-3] c.e.f.controller.ArticleController       : Article(id=1, title=가가가가가가가, content=나나나나나나나)
    
    요약
    롬복을 가지고 리팩토링 할 수 있더라
    리팩토링으로 코드를 압축시킬수도 있고
    출력하는 기능을 로깅기능으로 바꿀 수 있더라. 참 쉽죠?

--- src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -1,29 +1,16 @@
 package com.example.firstproject.dto;
 
 import com.example.firstproject.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor //생성자
+@ToString //toString()
+//Refactoring 했네 ~
 public class ArticleForm {
     
     private String title;
     private String content;
-
-    //생성자란? -> 인스턴스 초기화 메서드
-    // 변수의 초기화 작업에 주로 사용되어 인스턴스 생성 시에 실행되어야 할 작업을 위해서도 사용된다.
-    public ArticleForm(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }
-
-    //데이터가 잘 받아졌는지 확인하기 위해 toString() 메서드를 제너레이트 해준다.
-    //@Override란? -> 자식 클래스에 여러 개의 메서드가 있을 경우, 어떤 메서드가 Override 되었는지 쉽게 알기 어렵습니다.
-    //이 때, 오버라이드 메서드를 사용하여 명시적으로 선언하게 되면 많은 메서드 중에서 어떠한 메서드가 부모 클래스로부터 오버라이딩이 되었는지 쉽게 파악
-    @Override
-    public String toString() {
-        return "ArticleForm{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 
     public Article toEntity() {
         return new Article(null, title, content);
